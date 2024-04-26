5d09afdb9852fd7b9d6c4006ad9d715eb4b09189
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-04-26 14:15:56 +0700

    memasukan int nim

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -135,6 +135,7 @@ int main() {
 	{
 		try
 		{
+
 			cout << "1. Tambah data" << endl;
 			cout << "2. Hapus Data" << endl;
 			cout << "3. Tampilkan Data " << endl;
@@ -142,6 +143,35 @@ int main() {
 			cout << "5. Keluar" << endl; 
 			cout << "Pilihan:";
 			cin >> pilihan;
+			switch (pilihan)
+			{
+			case 1:
+				addNode();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List Kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+
+				int nim;
+				cout << "Masukkan NIM:";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim:" << nim << "berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+
+
+			}
 		}
 	}
 }
