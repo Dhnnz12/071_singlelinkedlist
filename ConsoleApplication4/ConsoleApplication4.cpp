55f2dd83e0597b3b7f4849034216c8cb63c963f7
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-04-26 14:10:36 +0700

    memasukan do dan try

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -131,4 +131,17 @@ void searchData() {
 
 int main() {
 	int pilihan;
+	do
+	{
+		try
+		{
+			cout << "1. Tambah data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tampilkan Data " << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl; 
+			cout << "Pilihan:";
+			cin >> pilihan;
+		}
+	}
 }
