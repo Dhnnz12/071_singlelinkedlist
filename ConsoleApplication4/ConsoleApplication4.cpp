76a192be34c4f2de8d3499dedd023d4d64c0daa7
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-04-26 13:45:58 +0700

    menambahkann if

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -22,7 +22,7 @@ void addNode() {
 	if (START == NULL || nim <= START->noMhs) {
 		if (START != NULL && nim == START->noMhs)
 		{
-			cout << "NIM sudah ada" << endl
+			cout << "NIM sudah ada" << endl;
 				return;
 		}
 
@@ -36,4 +36,16 @@ void addNode() {
 	Node* current = START;
 
 	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+				return;
+		}
+		previous = current;
+		current = current->next;
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
 }
